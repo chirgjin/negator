d1712987460cb23ad2bbf721972075a3a4035d7e
Sahil004 <sahilkrsingh004@gmail.com>
2019-01-09 03:37:23 +0530

    '.'

--- extension/app.js
+++ extension/app.js
@@ -14,5 +14,16 @@ window.onload = function () {
 
 
 $(".slider").change(function () {
-    console.log(this.value);
+    $('.percentage').html(this.value+'%')
+});
+
+$(".search").change(function () {
+    console.log(this.value)
+})
+
+$('.search').keypress(function(e) {
+    if (e.which == '13') {
+       e.preventDefault();
+       console.log(this.value)
+     }
 });

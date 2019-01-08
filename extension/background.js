cef5bda5214e5724c6cd484badb1b251f47f644a
Shreyans Jain <shreyansjain68@gmail.com>
2019-01-08 19:18:58 +0530

    extension

--- extension/background.js
+++ extension/background.js
@@ -15,3 +15,13 @@ chrome.runtime.onInstalled.addListener(function () {
         }]);
     });
 });
+
+document.addEventListener('load', function () {
+    chrome.tabs.query({
+        active: true,
+    }, function (tabs) {
+        chrome.tabs.executeScript(tabs[0].id, {
+            code:  "console.log('lol')"
+        });
+    });
+})
